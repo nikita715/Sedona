abf6f74bb5d0c44d0122a08ccae089639b64562e
nikita715 <nikstepbox@gmail.com>
2016-12-09 09:26:29 +0300

    Complete refactoring

--- pink/assets/js/script.js
+++ pink/assets/js/script.js
@@ -2,16 +2,16 @@
 $(document).ready(function () {
     'use strict';
     var main_navigation_opened = 0;
-    $('#main-navigation-opener').click(function () {
+    $('.main-nav-opener').click(function () {
         if (main_navigation_opened === 0) {
-            $('.main-navigation a').css('display', 'inline');
-            $('#main-navigation-opener').css('background-image', 'url(assets/img/menu-cross.png)');
+            $('.main-nav').css('display', 'flex');
+            $('.main-nav-opener').css('background-image', 'url(assets/img/menu-cross.png)');
             $('.main-header').css('background-color', '#283645');
             $('.main-header').css('background-image', 'none');
             main_navigation_opened = 1;
         } else {
-            $('.main-navigation a').css('display', 'none');
-            $('#main-navigation-opener').css('background-image', 'url(assets/img/menu-burger.png)');
+            $('.main-nav').css('display', 'none');
+            $('.main-nav-opener').css('background-image', 'url(assets/img/menu-burger.png)');
             $('.main-header').css('background-color', 'transparent');
             $('.main-header').css('background-image', 'url(assets/img/header-layer.png)');
             main_navigation_opened = 0;
